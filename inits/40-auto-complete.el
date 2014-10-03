104a994418ed2c78f82483daf21c9eb7c0a1bf9f
megane42 <h.kzm.gm@gmail.com>
2014-10-03 16:23:30 +0900

    web-modeとmarkdown-modeでauto-complete-modeが起動するようにした

--- inits/40-auto-complete.el
+++ inits/40-auto-complete.el
@@ -1,3 +1,7 @@
 (require 'auto-complete)
 (require 'auto-complete-config) ; 必須ではないですが一応
 (global-auto-complete-mode t)
+;; 追加メジャーモード
+(add-to-list 'ac-modes 'markdown-mode)
+(add-to-list 'ac-modes 'web-mode)
+
