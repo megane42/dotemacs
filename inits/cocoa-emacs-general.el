aadef6c1a600fdd83007ebaa71c07631e11ecba4
megane42 <h.kzm.gm@gmail.com>
2014-11-02 12:56:42 +0900

    Mac で emacsclient を使うように変更。付属の EmacsClient.app にテキストファイルを関連づければよい。

--- inits/cocoa-emacs-general.el
+++ inits/cocoa-emacs-general.el
@@ -1,3 +1,7 @@
 ;; Command-Key と Option-Key の役割を入れ替える
 (setq ns-command-modifier (quote meta))
 (setq ns-alternate-modifier (quote super))
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
