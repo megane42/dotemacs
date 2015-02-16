e0f4201e66bbb64d1fbd1c5849e5afaa8c47e905
kazama.hikaru-nif <kazama.hikaru@nifty.co.jp>
2015-02-16 09:40:09 +0900

    helm の設定をつめた

--- inits/40-helm.el
+++ inits/40-helm.el
@@ -1,11 +1,15 @@
-(helm-mode 1)
-(define-key global-map (kbd "C-x b")   'helm-buffers-list)
-(define-key global-map (kbd "C-x C-b") 'helm-for-files)
+(when (require 'helm-config nil t)
+  (helm-mode 1)
+  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-c C-f") 'helm-for-files)
+  (define-key global-map (kbd "C-c i")   'helm-imenu)
+  (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
 
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 
-(defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
-  "Execute command only if CANDIDATE exists"
-  (when (file-exists-p candidate)
-    ad-do-it))
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it)))
