39f363f645990678320d317daa7f7a21ac1bdf65
kazama.hikaru-nif <kazama.hikaru@nifty.co.jp>
2015-03-24 11:40:03 +0900

    ESC2回でESC1回の入力になるよう修正（vi対策）

--- inits/40-multi-term.el
+++ inits/40-multi-term.el
@@ -3,3 +3,12 @@
 ;; 奪われたくないキーバインドはここに書く
 (add-to-list 'term-unbind-key-list '"C-t")
 (add-to-list 'term-unbind-key-list '"C-q")
+
+;; ESC2回で term にESC1回分を直接送信
+;; 参考: http://fukuyama.co/zsh
+(add-hook 'term-mode-hook
+ (lambda ()
+   (define-key term-raw-map (kbd "ESC ESC")
+     (lambda ()
+       (interactive)
+       (term-send-raw-string "\e")))))
