f5762322954fd16ab3a93392b965fe0ca9473e37
megane42 <h.kzm.gm@gmail.com>
2018-01-05 02:37:42 +0000

    タブ文字を可視化するときの記号を変更

--- inits/10-whitespace-mode.el
+++ inits/10-whitespace-mode.el
@@ -1,6 +1,6 @@
 (use-package whitespace
   :config
-  (setq whitespace-style '(face         ; faceで可視化
+  (setq whitespace-style '(face           ; faceで可視化
                            trailing       ; 行末
                            tabs           ; タブ
                            spaces         ; スペース
@@ -9,15 +9,9 @@
                            tab-mark
                            ))
 
-
   (setq whitespace-display-mappings
         '((space-mark ?\u3000 [?\u25a1])
-          ;; WARNING: the mapping below has a problem.
-          ;; When a TAB occupies exactly one column, it will display the
-          ;; character ?\xBB at that column followed by a TAB which goes to
-          ;; the next TAB column.
-          ;; If this is a problem for you, please, comment the line below.
-          (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+          (tab-mark   ?\t [?\u002D ?\t] [?\\ ?\t])))
 
   ;; スペースは全角のみを可視化
   (setq whitespace-space-regexp "\\(\u3000+\\)")
